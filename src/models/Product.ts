063d7ee57ebc9d7526fac8ffff67bd97336f7f3a
Valenmar03 <valenmrtnz8@gmail.com>
2025-02-13 11:19:17 -0300

    add Product model with schema definition for productName, description, and stock

---
+++ src/models/Product.ts
@@ -0,0 +1,28 @@
+import mongoose, {Schema, Document, ProjectionType} from "mongoose";
+
+export type ProductType = Document & {
+    productName: string;
+    description: string;
+    stock: number;
+}
+
+
+const ProductSchema: Schema = new Schema({
+    productName: {
+        type: String, 
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String, 
+        required: true,
+        trim: true,
+    },
+    stock: {
+        type: Number, 
+        required: true,
+    },
+})
+
+const Product = mongoose.model<ProductType>("Product", ProductSchema)
+export default Product
